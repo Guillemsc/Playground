67cdce282597d8e2befd2a2cb2a381c4413ae279
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2021-07-20 21:05:54 +0200

    Hard pause

--- Assets/Playground/Scripts/Runtime/Utils/EditorSettings/FastEditorSettings.cs
+++ Assets/Playground/Scripts/Runtime/Utils/EditorSettings/FastEditorSettings.cs
@@ -5,6 +5,7 @@ namespace Playground.Utils.Materials
     public class FastEditorSettings : MonoBehaviour
     {
         [SerializeField] [Range(0, 999)] private int maxFramerate = 999;
+        [SerializeField] private bool vSync = default;
 
 #if UNITY_EDITOR
         private void Update()
@@ -16,6 +17,15 @@ namespace Playground.Utils.Materials
         private void SetSettings()
         {
             Application.targetFrameRate = maxFramerate;
+
+            if(vSync)
+            {
+                QualitySettings.vSyncCount = 1;
+            }
+            else
+            {
+                QualitySettings.vSyncCount = 0;
+            }
         }
     }
 }
---
+++ Assets/Playground/Scripts/Runtime/Utils/Pause/HardPause.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Playground.Debug
+{
+    public class HardPause : MonoBehaviour
+    {
+        private bool paused = false;
+
+        void Update()
+        {
+            if(Input.GetKeyDown("p"))
+            {
+                TogglePause();
+            }
+        }
+
+        private void TogglePause()
+        {
+            if(paused)
+            {
+                Time.timeScale = 1.0f;
+
+                paused = false;
+            }
+            else
+            {
+                Time.timeScale = 0.0f;
+
+                paused = true;
+            }
+        }
+    }
+}
