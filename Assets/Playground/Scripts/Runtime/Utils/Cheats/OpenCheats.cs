3ab70eb5c9d59e22a079843cf8f19bcd6f117284
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2021-07-22 23:20:35 +0200

    Open cheats helper

---
+++ Assets/Playground/Scripts/Runtime/Utils/Cheats/OpenCheats.cs
@@ -0,0 +1,16 @@
+﻿using SRDebugger;
+using UnityEngine;
+
+namespace Playground.Utils.Materials
+{
+    public class OpenCheats : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse2))
+            {
+                SRDebug.Instance.ShowDebugPanel(DefaultTabs.Options);
+            }
+        }
+    }
+}
