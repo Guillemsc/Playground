6b340b76fb8f5e4bd35d40517ce5f8332891ce5e
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2021-07-24 14:09:06 +0200

    Head

--- Assets/Playground/Scripts/Runtime/Content/Meta/UI/CarPanel/CarPanelUIViewModel.cs
+++ Assets/Playground/Scripts/Runtime/Content/Meta/UI/CarPanel/CarPanelUIViewModel.cs
@@ -10,6 +10,8 @@ namespace Playground.Content.Meta.UI.CarPanel
             = new ObservableEvent<PointerCallbacks, EventArgs>();
         public ObservableEvent<PointerCallbacks, EventArgs> OnCarSelectedEvent { get; }
             = new ObservableEvent<PointerCallbacks, EventArgs>();
+        public ObservableEvent<PointerCallbacks, EventArgs> OnCarPurchasedEvent { get; }
+            = new ObservableEvent<PointerCallbacks, EventArgs>();
 
         public ObservableVariable<string> CarNameVariable { get; }
             = new ObservableVariable<string>();
